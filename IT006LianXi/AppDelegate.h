1fff73dba413afe5bcad559480d392ee320a6023
MAC <MAC@MACdeiMac-3.local>
2016-06-01 09:41:27 +0800

    zxy
    add  project

---
+++ IT006LianXi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IT006LianXi
+//
+//  Created by MAC on 16/5/17.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IT006LianXi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IT006LianXi
+//
+//  Created by MAC on 16/5/17.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
